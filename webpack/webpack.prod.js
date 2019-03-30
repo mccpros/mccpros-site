e41f77af9103a087262061240ea2b6ef0cfc63f3
Denis Wells <deniswells59@gmail.com>
2019-03-30 14:12:30 -0700

    remove fsevents dependency

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -15,8 +15,8 @@ module.exports = merge(common, {
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
     new CompressionPlugin({
-      asset: "gzip_[path]",
-      algorithm: "gzip",
+      asset: 'gzip_[path]',
+      algorithm: 'gzip',
       test: /\.js$|\.css$|\.html$/,
       threshold: 10240,
       minRatio: 0.8
